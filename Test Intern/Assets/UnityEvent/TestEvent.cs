14a3b1dfbdbb18bad0a7611c648072bd9e69edb5
Trong Nguyen <watercup2020@outlook.com>
2020-06-29 09:33:53 +0700

    problem

--- Test Intern/Assets/UnityEvent/TestEvent.cs
+++ Test Intern/Assets/UnityEvent/TestEvent.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-
 public class TestEvent : MonoBehaviour
 {
+    [SerializeField] UnityEvent onClick;
+    [SerializeField] UnityEvent<string> onClickText;
+    [SerializeField] InputField inputField;
+
+    //for buttons
+    public void OnBtnClick()
+    {
+    }
+
+    public void OnBtnClickLogText()
+    {
+    }
+
+    //for event
+    public void LogBtnClick()
+    {
+        Debug.Log("Clicked");
+    }
+
+    public void LogInputField(string str)
+    {
+        Debug.Log(str);
+    }
 }
