5a3eabe24ed272a3c611576e12fe6a1df0993618
Trong Nguyen <watercup2020@outlook.com>
2020-06-29 15:07:07 +0700

    delegate

--- Test Intern/Assets/Delegate/DelegateTest.cs
+++ Test Intern/Assets/Delegate/DelegateTest.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class DelegateTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnBtnClick()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //gọi delegate
     }
 }
